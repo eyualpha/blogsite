e40722c120a99346f88b0960eaf492f12b65853c
eyualpha <eyuashenafi533@gmail.com>
2025-06-12 12:04:55 +0300

    implementd kinde authentication

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,16 @@
-export default function DashboardPage() {
-  return <div></div>;
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+const { getUser } = getKindeServerSession();
+
+export default async function DashboardPage() {
+  const session = await getUser();
+  if (!session) {
+    redirect("/api/auth/register");
+  }
+  return (
+    <div>
+      <h1>Dashbord</h1>
+    </div>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,21 @@
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const session = await getUser();
+
   return (
     <nav className="flex items-center justify-between py-4">
       <div className="flex items-center justify-between space-x-4">
         <Link href={"/"}>
-          <h1 className="text-2xl md:text-3xl font-bold text-gray-800">
+          <h1 className="text-2xl md:text-3xl font-bold ">
             Blog<span className="text-orange-500">Site</span>
           </h1>
         </Link>
@@ -20,10 +29,19 @@ export default function Navbar() {
         </Link>
       </div>
 
-      <div className="space-x-4">
-        <Button variant={"outline"}>Login</Button>
-        <Button>Sign Up</Button>
-      </div>
+      {session ? (
+        <div className="flex items-center gap-4">
+          {session.given_name}
+          <LogoutLink className={buttonVariants()}>Logout</LogoutLink>
+        </div>
+      ) : (
+        <div className="space-x-4">
+          <LoginLink className={buttonVariants({ variant: "secondary" })}>
+            Login
+          </LoginLink>
+          <RegisterLink className={buttonVariants()}>Sign Up</RegisterLink>
+        </div>
+      )}
     </nav>
   );
 }
